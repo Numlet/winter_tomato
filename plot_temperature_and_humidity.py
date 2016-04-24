fe053da9ed18af446d435669fafc4f64992fb7f3
Jesus <eejvt@leeds.ac.uk>
2016-04-24 17:19:50 +0100

    python scripts added for logging temp and plotting

---
+++ plot_temperature_and_humidity.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+saving_file='temperature_log_%s.csv'%months_str_upper_case[datetime.date.today().month-1]
+
+
+data=np.genfromtxt(saving_file)
+if len(data[:,0])>13000:
+    data=np.copy(data[-13000:,:])
+date_number=data[:,0]
+date_str=data[:,1]
+temperature=data[:,2]
+humidity=data[:,3]
+socket_on=data[:,4]
+plt.figure()
+f, (ax1, ax2,ax3) = plt.subplots(3, sharex=True)
+ax1.plot(date_str,temperature)
+ax1.set_title('Temperature (top) and Humidity')
+ax2.plot(date_str,humity)
+ax3.plot(date_str,socket_on)
+# Fine-tune figure; make subplots close to each other and hide x ticks for
+# all but bottom plot.
+f.subplots_adjust(hspace=0)
+plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+plt.savefig('last_week.png')
---
+++ temperature_log.py
@@ -0,0 +1,24 @@
+import datetime
+import time
+import sys
+import Adafruit_DHT
+
+months_str_upper_case=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+
+pin_number=21
+
+saving_file='temperature_log_%s.csv'%months_str_upper_case[datetime.date.today().month-1]
+
+def get_date_str()
+    date_now=datetime.datetime.now()
+    date_formatted=str(date_now.day)+'/'+str(date_now.month)+' '+str(date_now.hour)+':'+str(date_now.minute)+':'+str(date_now.second)
+    return date_formatted
+
+socket_on=0
+
+while True:
+    humity, temperature=Adafruit_DHT.read_retry(Adafruit_DHT.DHT11,pin_number)
+
+    line='\n'+str(time.time())+','+get_date_str()+','+str(temperature)+','+str(humity)+','+str(socket_on)
+    with open(saving_file,'a') as my_file:my_file.write(line)
+    time.sleep(5)

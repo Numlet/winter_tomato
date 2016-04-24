47553334cca2f3f43c54e3ae86e77ed069193e5d
Jesus - Raspberry <jsus.vergara@gmail.com>
2016-04-24 19:33:43 +0100

    bugs solved in plotting function

--- plot_temperature_and_humidity.py
+++ plot_temperature_and_humidity.py
@@ -14,7 +14,7 @@ date_str=data[:,1]
 temperature=data[:,2]
 humidity=data[:,3]
 socket_on=data[:,4]
-time_list=[datetime.datetime.fromtimestamp(i).__str__[8:-7] for i in date_number]
+time_list=[datetime.datetime.fromtimestamp(i) for i in date_number]
 print time_list
 print date_number
 print temperature
@@ -24,11 +24,12 @@ ax1.plot(time_list,temperature,'r')
 ax1.set_ylim(0,35)
 ax1.set_title('Temperature (top), Humidity (mid) and socket on/off (bot)')
 ax2.plot(time_list,humidity)
-ax1.set_ylim(0,100)
+ax2.set_ylim(0,100)
 ax3.plot(time_list,socket_on,'k--')
-ax1.set_ylim(0,1)
+ax3.set_ylim(0,1)
 # Fine-tune figure; make subplots close to each other and hide x ticks for
 # all but bottom plot.
 #f.subplots_adjust(hspace=0)
 plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+plt.gcf().autofmt_xdate()
 plt.savefig('last_week.png')
